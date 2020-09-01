b4ef59dfb99dcccbe46a5b2c9079fc8c1936bc73
François Huszti <f.huszti@gmail.com>
2020-09-02 01:02:07 +0200

    Fix d'un bug concernant le JSS et le SSR

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import ServerStyleSheets from "@material-ui/styles/ServerStyleSheets";
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
-	
 	render() {
 		return (
-			<Html>
+			<Html lang="fr">
 				<Head>
 					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
 					<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
@@ -22,5 +18,23 @@ class MyDocument extends Document {
 		)
 	}
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+	// On s'assure que le front et le serveur s'accordent sur le JSS
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
+	
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+		});
+	
+	const initialProps = await Document.getInitialProps(ctx);
+	
+	return {
+		...initialProps,
+		styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+	};
+};
 
 export default MyDocument;
