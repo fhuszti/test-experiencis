ee1e4814c137f53bad4915d82504edda618ead16
François Huszti <f.huszti@gmail.com>
2020-09-02 00:00:47 +0200

    Ajout MaterialUI et boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		//Retire le CSS injecté côté serveur
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles)
+			jssStyles.parentElement.removeChild(jssStyles);
+	}, []);
+	
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</React.Fragment>
+	);
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
 			<Head>
 				<title>Connexion | Test Experiencis</title>
 				<link rel="icon" href="/favicon.png"/>
+				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
 			</Head>
 			
 			<main>
