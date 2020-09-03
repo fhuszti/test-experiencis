85ade17bac7cb0a8cd266e1d8d2ffca67ebd3787
François Huszti <f.huszti@gmail.com>
2020-09-03 02:07:06 +0200

    Nettoyage imports test index + regeneration snapshot

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
 import renderer from 'react-test-renderer';
 import Home from "../../pages/index";
 
-let container = null;
-beforeEach(() => {
-	//On crée un élément du DOM comme cible pour le render pour chaque test
-	container = document.createElement("div");
-	document.body.appendChild(container);
-});
-
-afterEach(() => {
-	//On nettoie après chaque test
-	unmountComponentAtNode(container);
-	container.remove();
-	container = null;
-});
-
-it("render correctement", () => {
-	const tree = renderer.create(<Home/>).toJSON();
-	expect(tree).toMatchSnapshot();
+describe("index snapshot testing", () => {
+	it("renders correctly", () => {
+		const tree = renderer.create(<Home/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 });
